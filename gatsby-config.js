934249a721f87b7f9909110ad32b721969888106
Alvira Amedee <140256805+alviraamedee@users.noreply.github.com>
2023-07-22 23:35:29 +0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,8 +9,8 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Watch Full {[Barbie 2023]} Movie - A Dreamlike Adventure with Barbie and Her Friends`,
+    description: `Barbie, the iconic doll created by Mattel, has been captivating hearts around the globe since her debut in 1959. Her everlasting popularity led to the creation of a series of enchanting animated movies that continue to mesmerize audiences of all ages. In this article, we delve into the mesmerizing world of Barbie and explore the highly anticipated movie, Barbie 2023. Barbie has remained a beloved figure for generations, transcending time and cultural boundaries. With her impeccable style, charisma, and empowering persona, she has inspired countless young minds to dream big and believe in themselves. Over the years, Barbie movies have evolved, not just in animation quality but also in storytelling. From fairy tales to modern adventures, each movie carries a unique message while preserving the essence of Barbie's spirit.`,
     author: `@gatsbyjs`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
